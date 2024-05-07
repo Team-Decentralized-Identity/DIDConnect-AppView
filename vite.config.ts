190233e8b09d8419b5cd2ec0f88b31c0d11a283c
JN2402 <103538165+JN2402@users.noreply.github.com>
2024-05-07 17:44:24 -0500

    Update vite.config.ts
    changes to deploy on digital ocean server

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/skylight/",
+  server: {
+    host:'0.0.0.0' // listen on all network interfaces
+  }, //allows for the app view to be deployed on the digital ocean server
+ base: "/skylight/",
   plugins: [vue()],
   resolve: {
     alias: {
       "@/": `${__dirname}/src/`,
     },
   },
-});
+
+}
+);
